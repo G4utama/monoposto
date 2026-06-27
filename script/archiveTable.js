e50e42857221dee2de94d0791478d322cecdfab4
leonardo <baldoleonardo01@gmail.com>
2026-06-27 12:04:42 +0200

    update archive

--- script/archiveTable.js
+++ script/archiveTable.js
@@ -11,46 +11,30 @@ const createArchiveTable = async () => {
             ${year.map(data => `
                 ${data.era ? `<tr><td colspan="7">${data.era}</td></tr>` : ''}
                 <tr>
-                    <td>
-                        <p>${data.drivers[2].slice(0, 3)}</p>
-                        <img src="../assets/drivers/teams/${data.drivers[2].slice(4, 7)}.png" alt="driver">
-                        <p>${data["drivers-points"][2]}</p>
-                        <p>(-${data["drivers-points"][0]-data["drivers-points"][2]})</p>
+                    <td style="background: rgba(${data.driverColor.slice(4, -1)}, 0.25)">
+                        <p>${data.driver.slice(4, 7)}</p>
+                        <img src="../assets/teams/${data.driver.slice(4, 7)}-car.png">
                     </td>
-                    <td>
-                        <p>${data.drivers[1].slice(0, 3)}</p>
-                        <img src="../assets/drivers/teams/${data.drivers[1].slice(4, 7)}.png" alt="driver">
-                        <p>${data["drivers-points"][1]}</p>
-                        <p>(-${data["drivers-points"][0]-data["drivers-points"][1]})</p>
+                    <td style="background: rgba(${data.driverColor.slice(4, -1)}, 0.25)">
+                        <img src="../assets/flag/${data.driverFlag}.png">
                     </td>
-                    <td style="background: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(${data["driver-color"].slice(4, -1)}, 0.25)">
-                        <p>${data.drivers[0].slice(0, 3)}</p>
-                        <img src="../assets/drivers/teams/${data.drivers[0].slice(4, 7)}.png" alt="driver">
-                        <p >${data["drivers-points"][0]}</p>
+                    <td style="background: rgba(${data.driverColor.slice(4, -1)}, 0.25)">
+                        <p>${data.driver.slice(0, 3)}</p>
+                        <img src="../assets/drivers/teams/${data.driver.slice(4, 7)}.png">
                     </td>
-                    <td style="background: linear-gradient(to right, rgba(${data["driver-color"].slice(4, -1)}, 0.25), rgba(${data["team-color"].slice(4, -1)}, 0.25)">
-                        ${data.number}
+                    <td style="background: linear-gradient(to right, rgba(${data.driverColor.slice(4, -1)}, 0.25), rgba(${data.teamColor.slice(4, -1)}, 0.25)">
+                        ${data.year}
                     </td>
-                    <td style="background: linear-gradient(to right,  rgba(${data["team-color"].slice(4, -1)}, 0.25), rgba(0, 0, 0, 0)">
-                        <p>${data.teams[0]}</p>
-                        <img src="../assets/teams/${data.teams[0]}-car.png" alt="team">
-                        <p>${data["teams-points"][0]}</p>
+                    <td style="background: rgba(${data.teamColor.slice(4, -1)}, 0.25)">
+                        <p>${data.team}</p>
+                        ${data.team ? `<img src="../assets/teams/${data.team}-car.png">` : ''}
                     </td>
-                    <td>
-                        <p>${data.teams[1]}</p>
-                        <img src="../assets/teams/${data.teams[1]}-car.png" alt="team">
-                        <p>${data["teams-points"][1]}</p>
-                        <p>(-${data["teams-points"][0]-data["teams-points"][1]})</p>
+                    <td style="background: rgba(${data.teamColor.slice(4, -1)}, 0.25)">
+                        ${data.teamFlag ? `<img src="../assets/flag/${data.teamFlag}.png">` : ''}
                     </td>
-                    <td>
-                        <p>${data.teams[2]}</p>
-                        <img src="../assets/teams/${data.teams[2]}-car.png" alt="team">
-                        <p>${data["teams-points"][2]}</p>
-                        <p>(-${data["teams-points"][0]-data["teams-points"][2]})</p>
-                    </td>
+                    
                 </tr>
             `).join('')}
-            <tr><td colspan="7">...</td></tr>
         </table>
     `;
     
